42b0d1388b45560714ef306fe96fb4e6d019c729
Toshiyasu kondou <toshiyasukondou@ToshiyasunoMacBook-Air.local>
2021-11-06 03:10:20 +0900

    課題2修正１

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const button = document.getElementById("output");
-const fizzbuzzList = document.getElementById("fizzbuzzList");
+const button = document.getElementById('output');
+const fizzbuzzList = document.getElementById('fizzbuzzList');
 
-
-const listAdd = (a) => {
-  const li = document.createElement("li");
-  li.innerHTML = a;
+const listAdd = (fizzbuzzOutput) => {
+  const li = document.createElement('li');
+  li.innerHTML = fizzbuzzOutput;
   fizzbuzzList.appendChild(li);
 };
 
-button.addEventListener("click", () => {
-  const fizzNum = Number(document.getElementById("fizzNum").value);
-  const buzzNum = Number(document.getElementById("buzzNum").value);
-  fizzbuzzList.innerHTML = "";
+button.addEventListener('click', () => {
+  const fizzNum = Number(document.getElementById('fizzNum').value);
+  const buzzNum = Number(document.getElementById('buzzNum').value);
+  fizzbuzzList.innerHTML = '';
   
   if (
     Number.isInteger(fizzNum) &&
@@ -19,16 +18,17 @@ button.addEventListener("click", () => {
     fizzNum > 0 &&
     buzzNum > 0
   ) {
+    
     for (i = 1; i < 100; i++) {
       if (i % fizzNum === 0 && i % buzzNum === 0) {
-        listAdd("FizzBuzz" + i);
+        listAdd(`FizzBuzz ${i}`);
       } else if (i % fizzNum === 0) {
-        listAdd("Fizz" + i);
+        listAdd(`Fizz ${i}`);
       } else if (i % buzzNum === 0) {
-        listAdd("Buzz" + i);
+        listAdd(`Buzz ${i}`);
       }
     }
   } else {
-    listAdd("整数値を入力してください");
+    listAdd('整数値を入力してください');
   }
 });
